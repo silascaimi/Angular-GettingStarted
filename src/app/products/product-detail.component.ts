9be2b62eb0b79b252bd7204fdb6eb0cae529afb8
Silas Caimi <silascaimi@live.com>
2020-11-14 19:02:38 -0300

    add product object to mock

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -16,5 +16,16 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
     let id = +this.route.snapshot.paramMap.get('id');
     this.pageTitle += `: ${id}`;
+
+    this.product = {
+      productId: 1,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2019',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    };
   }
 }
