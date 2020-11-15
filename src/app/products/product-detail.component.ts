395963516ac931191ee17996da3295434ac2594c
Silas Caimi <silascaimi@live.com>
2020-11-14 23:34:29 -0300

    implement product detail and create service to request product by id

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,23 +11,22 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
+  errorMessage: string;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     let id = +this.route.snapshot.paramMap.get('id');
     this.pageTitle += `: ${id}`;
 
-    this.product = {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2019',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    };
+    this.productService.getProduct(id).subscribe({
+      next: (product) => (this.product = product),
+      error: (err) => (this.errorMessage = err),
+    });
   }
 
   onBack(): void {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { IProduct } from './product';
 
@@ -17,6 +17,12 @@ export class ProductService {
     return this.http.get<IProduct[]>(this.productUrl).pipe(
       tap((data) => console.log('All: ' + JSON.stringify(data))),
       catchError(this.handleError)
+    );
+  }
+
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts().pipe(
+      map((products: IProduct[]) => products.find((p) => p.productId === id))
     );
   }
 
