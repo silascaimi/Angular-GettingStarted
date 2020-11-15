d7677475d50beacaadd395c6ce3525104d5a8200
Silas Caimi <silascaimi@live.com>
2020-11-15 13:39:11 -0300

    create product-routing module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,22 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
-import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
+import { ProductRoutingModule } from './product-routing.module';
 
 @NgModule({
   declarations: [ProductListComponent, ProductDetailComponent],
-  imports: [
-    RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
-      {
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent,
-      },
-    ]),
-    SharedModule,
-  ],
+  imports: [SharedModule, ProductRoutingModule],
 })
 export class ProductModule {}
