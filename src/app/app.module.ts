2f701088c18b9a82f79eca88cd2880ec5fd3c3e5
Silas Caimi <silascaimi@live.com>
2020-11-15 13:31:06 -0300

    create a app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, WelcomeComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
-    ]),
-    ProductModule,
-  ],
+  imports: [BrowserModule, HttpClientModule, ProductModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
